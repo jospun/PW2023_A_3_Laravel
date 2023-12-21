b72e0476f9d01caafdae5c7ef0aee02240011049
Carolus Seto <120619971+WIBU1234@users.noreply.github.com>
2023-12-21 11:28:13 +0700

    Final keknya

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -4,7 +4,9 @@
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://use.fontawesome.com/f59bcd8580.js"></script>
-    <title>FEST fes</title>
+    <title>CAJ</title>
+    <link rel="icon" href="images\Export.png" type="image/x-icon">
+
     <style>
         body {
             background: #eeeeee;
